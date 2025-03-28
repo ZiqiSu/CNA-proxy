e2d282970ab379a71993b2adf87b68ab252995f8
ZiqiSu <100886355+ZiqiSu@users.noreply.github.com>
2025-03-29 00:36:03 +1030

    Add files via upload
    First step to create the socket and listen

--- Proxy.py
+++ Proxy.py
@@ -20,6 +20,7 @@ proxyPort = int(args.port)
 try:
   # Create a server socket
   # ~~~~ INSERT CODE ~~~~
+  serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Use the normal IPV4 web connectto the TCP
   # ~~~~ END CODE INSERT ~~~~
   print ('Created socket')
 except:
@@ -29,6 +30,7 @@ except:
 try:
   # Bind the the server socket to a host and port
   # ~~~~ INSERT CODE ~~~~
+  serverSocket.bind((proxyHost, proxyPort)) #Bind a host and port
   # ~~~~ END CODE INSERT ~~~~
   print ('Port is bound')
 except:
@@ -38,6 +40,7 @@ except:
 try:
   # Listen on the server socket
   # ~~~~ INSERT CODE ~~~~
+  serverSocket.listen(8) #Listen the socket
   # ~~~~ END CODE INSERT ~~~~
   print ('Listening to socket')
 except:
@@ -52,6 +55,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
+    clientSocket, clientAddress = serverSocket.accept() #Accept from the client
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
